5eae4db4620244eb51fd4e936f8d1ebf51fe3d35
ElvisLehtse <elehtse@gmail.com>
2024-07-22 15:11:02 +0300

    added components

---
+++ cardgame-front/src/components/UserChoiceForms.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserChoiceForms(props) {
+  return (
+    <form onSubmit={props.submit}>
+      <button onClick={() => props.choose("lower")} type="submit" className="btn btn-primary mt-3" disabled={props.status}>Lower</button>
+      <button onClick={() => props.choose("equal")}  type="submit" className="btn btn-primary mt-3" disabled={props.status}>Equal</button>
+      <button onClick={() => props.choose("higher")}  type="submit" className="btn btn-primary mt-3" disabled={props.status}>Higher</button>
+    </form>
+  )
+}
+
+export default UserChoiceForms
